b381e5c102ad1c78644f726bc328486a428978cc
Michal Skrzypczynski <276157@student.pwr.edu.pl>
2024-12-07 09:15:58 +0100

    initial commit

--- se_app/urls.py
+++ se_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from .views import ProductViewSet, CustomerViewSet, OrderViewSet
 
 router = DefaultRouter()
@@ -9,5 +11,7 @@ router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
 path('api/', include(router.urls)),
+path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+path('api/token/refresh/', TokenRefreshView.as_view(),name='token_refresh'),
 ]
 
