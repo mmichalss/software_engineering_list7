c1ddfdc90a02f8ecf9455bcf767469c334b977c8
Michal Skrzypczynski <276157@student.pwr.edu.pl>
2025-01-11 14:40:18 +0100

    list9part3

--- se_app/views.py
+++ se_app/views.py
@@ -3,11 +3,14 @@ from rest_framework import viewsets
 from .serializers import ProductSerializer, CustomerSerializer, OrderSerializer
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdminOrReadOnly
+from rest_framework.filters import SearchFilter
 
 class ProductViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ['name']
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
