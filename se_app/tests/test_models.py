850c446930f7ff8c6bb364dfbed378d990cf1b0f
Michal Skrzypczynski <276157@student.pwr.edu.pl>
2025-01-18 14:20:34 +0100

    next step

--- se_app/tests/test_models.py
+++ se_app/tests/test_models.py
@@ -11,7 +11,7 @@ class ProductModelTest(TestCase):
     def test_create_product_with_valid_data(self):
         temp_product = Product.objects.create(name='Temporary product', price=1.99, available=True)
         self.assertEqual(temp_product.name, 'Temporary product')
-        self.assertEqual(temp_product.price, 1.98)
+        self.assertEqual(temp_product.price, 1.99)
         self.assertTrue(temp_product.available)
 
     def test_create_product_with_negative_price(self):
