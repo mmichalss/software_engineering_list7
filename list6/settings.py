607ba1d4f918723ae8844acb9a4bec7f4dec21ea
Michal Skrzypczynski <276157@student.pwr.edu.pl>
2025-01-18 11:19:12 +0100

    next attempt

--- list6/settings.py
+++ list6/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         "NAME": os.getenv("DATABASE_NAME"),
         "USER": os.getenv("DATABASE_USER"),
         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-        "HOST": os.getenv("DATBASE_HOST", "localhost"),
+        "HOST": os.getenv("DATABASE_HOST", "localhost"),
         "PORT": os.getenv("DATABASE_PORT", 5432),
     }
 }
